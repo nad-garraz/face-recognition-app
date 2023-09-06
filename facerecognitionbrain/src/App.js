74e8a0b96c71874845fe77b4179e7cb685b73390
nad-garraz <nad.neutron@proton.me>
2023-09-06 01:09:00 -0300

    build with no process.env

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch(process.env.BACKEND_URL + '/imageurl', {
+    fetch('https://mybackendfrecon.onrender.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
